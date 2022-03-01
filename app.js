b55fd0cceb873a6b072b5add30c318f5fa65159b
Torben Storm Jensen <tstormdk@gmail.com>
2022-03-01 12:12:22 +0100

    All inclusive

---
+++ app.js
@@ -0,0 +1,29 @@
+const promises = [];
+
+const displayPokemon = pokemon => {};
+
+for (let i = 1; i <= 20; i++) {
+  const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
+  promises.push(fetch(url).then(res => res.json()));
+}
+Promise.all(promises).then(results => {
+  console.log(results);
+  
+  const pokemon = results.map(data => ({
+    name: data.name,
+    id: data.id,
+    image: data.sprites["front_default"],
+    type: data.types.map(type => type.type.name).join(", "),
+  }));
+  displayPokemon(pokemon);
+  
+  const pokemonHTMLString = pokemon
+  .map(
+    pokeman =>
+      ` <li class="card"> <img class="card-image" src="${pokeman.image}"/> <h2 class="card-title">${pokeman.id}. ${pokeman.name}</h2> <p class="card-subtitle">Type: ${pokeman.type}</p> </li> `
+  )
+  .join("");
+  const pokedex = document.getElementById("pokedex");
+  pokedex.innerHTML = pokemonHTMLString;
+});
+
